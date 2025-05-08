df9e8e4f93c8cf0b2948a2bc9b1e9635a45d52f2
islam0122 <duishobaevislam01@gmaul.com>
2025-05-09 05:10:45 +0600

    day_02

---
+++ WEEK_01/day_02/day_02.py
@@ -0,0 +1,44 @@
+print('Mercury', 'Venus', sep='*', end='!')
+print('Mars', 'Jupiter', sep='**', end='?')
+print("\n________________________________________")
+sep1 = '#'
+end2 = '?'
+print('YES', 'NO', sep=sep1, end='+')
+print('MAYBE', 'NEVER', sep='*', end=end2)
+print("\n________________________________________")
+
+print('a', 'b', 'c', sep='*')
+print('d', 'e', 'f', sep='**', end='')
+print('g', 'h', 'i', sep='+', end='%')
+print('j', 'k', 'l', sep='-', end='\n')
+print('m', 'n', 'o', sep='/', end='!')
+print('p', 'q', 'r', sep='1', end='%')
+print('s', 't', 'u', sep='&', end='\n')
+print('v', 'w', 'x', sep='%')
+print('y', 'z', sep='/', end='!')
+print("\n________________________________________")
+
+print('БЕСКОНЕЧНОСТЬ', 'НЕ', 'ПРЕДЕЛ', '!', sep='_')
+
+print('БЕСКОНЕЧНОСТЬ_НЕ_ПРЕДЕЛ!')
+
+print('БЕСКОНЕЧНОСТЬ', 'НЕ', 'ПРЕДЕЛ!', sep='_', end='!')
+
+print('БЕСКОНЕЧНОСТЬ', 'НЕ', 'ПРЕДЕЛ', sep='_', end='')
+print('!')
+
+print('БЕСКОНЕЧНОСТЬ_НЕ_ПРЕДЕЛ', end='')
+print('!')
+
+print('БЕСКОНЕЧНОСТЬ_', 'НЕ_', 'ПРЕДЕЛ!', sep='', end='')
+
+print("\n________________________________________")
+
+# print('Java')
+# print('Ruby')
+# print('Scala')
+print('Python', end='+')  # print('C++')
+# print('GO')
+print('C#', end='=')  # print('C')
+print('awesome')
+# finish
