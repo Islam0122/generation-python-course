a6e284f6ee5bea09045f521ae7b57bc638891bb1
islam0122 <duishobaevislam01@gmaul.com>
2025-05-08 06:49:38 +0600

    day_01

---
+++ WEEK_01/day_01/day_01.py
@@ -0,0 +1,10 @@
+# # Просим пользователя ввести имя
+# name = input("Введите ваше имя: ")
+# # Выводим приветствие
+# print(f"Привет, {name}!")
+
+
+age = int(input('Enter your age: '))
+year = 2025
+birth_year = year - age
+print(f"You were born in {birth_year}")
